1537ebb75dc5b5de87254b2fe8b9f4734704aa47
Mitch Fox <39552084+mitchfox@users.noreply.github.com>
2019-04-08 16:37:10 +1000

    update/save

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App() {
+  const headlines = [
+    { title: "My First Title", url: "https://news.com/first-title" },
+    { title: "My Second Title", url: "https://news.com/second-title" },
+    { title: "My Third Title", url: "https://news.com/third-title" },
+    { title: "My Fourth Title", url: "https://news.com/fourth-title" }
+  ];
   return (
     <div className="App">
-      <h1>News Headlines</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {headlines.map(headline => (
+        // `headline` is now an object
+        <Headline key={headline.url} title={headline.title} />
+      ))}
     </div>
   );
 }
